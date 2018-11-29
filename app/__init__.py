36f08fc133ff9400572d05fa36da47cab605462a
mwangi <sam56dvp@gmail.com>
2018-11-29 11:52:45 +0300

    [starts #162267646)]created method to facilitate endpoint for getting sspecific incident

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,15 @@ from flask_restful import Api
 
 #local imports
 from instance.config import app_config
-from app.api.v1.views.incident import Redflag
+from app.api.v1.views.incident import Redflag, Get_incident
 
 def create_app(config):
     """this method creates all instance needed for endpoints"""
     app = Flask(__name__)
     api = Api(app)
     app.config.from_object(app_config[config])
-    api.add_resource(Redflag, '/api/v1/records')
-        
+    api.add_resource(Redflag, '/api/v1/incidents')
+    api.add_resource(Get_incident, '/api/v1/incident/<int:id>')
+
+            
     return app  
--- app/api/v1/views/incident.py
+++ app/api/v1/views/incident.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, reqparse
 #local imports
 from app.api.v1.models import incidents, Models
 
+model = Models()
 
 class Redflag(Resource, Models):
     """redflag endpoints """
@@ -13,7 +14,7 @@ class Redflag(Resource, Models):
             return ({'message': 'no records found'}), 200
         return ({'incidents': self.db}), 200
 
-    def post(self):
+    def post(self): 
         """create a redflag"""
         parser = reqparse.RequestParser()
         parser.add_argument('comment', 
@@ -21,15 +22,40 @@ class Redflag(Resource, Models):
         required=True, 
         help='provide a comment')
 
+        
+        parser.add_argument('type_of_incident', 
+        type=str, 
+        required=True, 
+        help='provide type of incident')
+
+
         parser.add_argument('location',
         type=str,
-        required=True, 
+        required=True,
         help='please provide location')
         args = parser.parse_args()
-        Models().add_record(args)
+        # model.add_record(args)
 
-        return ({'Record': args}),201
+        data = {'comment': args['comment'], 
+        'location':args['location'],
+        'type_of_incident':args['type_of_incident']}
 
-   
+        incident_data = model.add_record(data)
+
+
+        return ({'status':'success','Incident': data}),201
+
+class Get_incident(Resource):
+    specific_incident = model.get_specif_record(id)
+    """class to get specific record"""
+
+    def get(self, id):
+        if model.get_specif_record(id):
+            return ({'status': "ok",
+                "Incident":model.get_specif_record(id)}),200
+        else: 
+            return ({"status":"success",
+                "incident": 'incident not found'}),200
+
 
 
