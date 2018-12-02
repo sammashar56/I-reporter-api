bafd22221f66474bbce9437c92beb01250b88853
mwangi <sam56dvp@gmail.com>
2018-12-02 05:54:05 +0300

    [finishes #162365866] user registration

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,11 +1,13 @@
 incidents = []
+users = []
 
 from datetime import datetime as dt
 
 class Models():
     def __init__(self):
+        self.users = users
         self.db = incidents
-
+        
     def add_record(self, data):
         id = len(self.db)+1
         data['id'] = id
@@ -13,14 +15,23 @@ class Models():
         data['creation date'] = dt.now().strftime(
                 '%Y-%m-%d %H:%M:%S')
         data['status'] = None
+        
         self.db.append(data)
+
+    def add_user(self, data):
+        id = len(self.users)+1
+        data['id'] = id
+        self.users.append(data)
 
     def get_specif_record(self, id):
         ident = [incident for incident in self.db if incident['id'] == id]
         return ident
-
+        
     def delete_incident(self, id):
         for i in incidents:
             if i['id'] == id:
                 incidents.remove(i)
                 
+        
+    def get_all_incidents(self):
+        return self.db
